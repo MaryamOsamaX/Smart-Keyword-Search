c5ed8dc0e7997717ceb66d6c8029a463944fc6d0
Kanekikid <ysamir1998@gmail.com>
2020-08-01 13:39:15 +0200

    Structure Updated

---
+++ CompetitorsKeywords.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  1 13:20:21 2020
+
+@author: Kaneki Kiddo
+"""
+
+def getKeywordsofCompetitor(url):
+    keywords = []
+    
+    ##########################
+    '''
+        code to be added here
+    '''
+    #########################
+    return keywords
