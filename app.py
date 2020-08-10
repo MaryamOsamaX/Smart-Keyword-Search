2a1a7914ca205d7d4870c1af5b4b8e2fe2eb5592
NOURhelmy <nourhan.m34@yahoo.com>
2020-08-10 21:34:16 +0200

    finalGUI

--- app.py
+++ app.py
@@ -9,28 +9,29 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 # 1
 @app.route('/')
 def hello_world():
-    return render_template('Home.html')
+    return render_template('index.html')
 
+@app.route("/homepage.html", methods=['POST', 'GET'])
+def home():
+    return render_template('homepage.html')
 
-@app.route("/keywords/", methods=['POST', 'GET'])
+@app.route("/keywords.html", methods=['POST', 'GET'])
 def findKeywords():
     if request.method == 'POST':
         result = request.form
         url = result['URL']
         keywords = Keywords_Search.findKeywords(url)
-
         return render_template('keywords.html', result=keywords)
     return render_template('keywords.html')
 
 
-@app.route("/competitors/", methods=['POST', 'GET'])
+@app.route("/competitors.html", methods=['POST', 'GET'])
 def findCompetitors():
     if request.method == 'POST':
         result = request.form
         url = result['URL']
         title, article, k_1 = scrap(url)
         competitors = Competitor_Main.get_comps(url, title, article)
-
         return render_template('competitors.html', result=competitors)
     return render_template('competitors.html')
 
